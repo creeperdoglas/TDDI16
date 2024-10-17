979c8e0a5f1a0667cf99dc6efc30949b78a24ed2
creeperdoglas <borenmelkan@gmail.com>
2024-10-17 17:05:10 +0200

    maybe :D

--- lab3/wordchain.cpp
+++ lab3/wordchain.cpp
@@ -2,226 +2,147 @@
 #include <string>
 #include <vector>
 #include <queue>
-#include <set>
 #include <unordered_set>
 #include <algorithm>
 
-using std::cout;
-using std::endl;
-using std::string;
-using std::vector;
-
 using namespace std;
+
 struct Node
 {
-    Node(string const &word)
-        : word{word} {}
-    Node(string const &word, Node *prev)
-        : word{word}, prev{prev} {}
     string word;
-    vector<string> neighbors;
-    Node *prev;
+    Node *prev; // Pekar på föregående nod i ordkedjan
+    Node(string const &word, Node *prev = nullptr) : word(word), prev(prev) {}
 };
 
 typedef unordered_set<string> Dictionary;
 string const alphabet{"abcdefghijklmnopqrstuvwxyz"};
 
-void find_neighbors(const Dictionary &dict, Node &node)
+// Hjälpfunktion som hittar grannar till ett ord genom att byta ut varje bokstav och kolla om det nya ordet finns i ordlistan.
+void find_neighbors(const Dictionary &dict, Node &node, vector<string> &neighbors)
 {
-    for (int i{}; i < static_cast<int>(node.word.size()); ++i)
+    for (int i = 0; i < static_cast<int>(node.word.size()); ++i)
     {
         string word = node.word;
         for (const char &c : alphabet)
         {
-            word.at(i) = c;
-            if (word != node.word)
+            word[i] = c;
+            if (word != node.word && dict.find(word) != dict.end())
             {
-                auto it = dict.find(word);
-                if (it != dict.end())
-                    node.neighbors.push_back(word);
+                neighbors.push_back(word);
             }
         }
     }
 }
 
-// Typ som används för ordlistan. Den definieras med en typedef här så att du enkelt kan ändra
-// representationen av en ordlista utefter vad din implementation behöver. Funktionen
-// "read_questions" skickar ordlistan till "find_shortest" och "find_longest" med hjälp av denna
-// typen.
-// typedef vector<string> Dictionary;
-
-/**
- * Hitta den kortaste ordkedjan från 'first' till 'second' givet de ord som finns i
- * 'dict'. Returvärdet är den ordkedja som hittats, första elementet ska vara 'from' och sista
- * 'to'. Om ingen ordkedja hittas kan en tom vector returneras.
- */
-// vector<string> find_shortest(Dictionary dict, const string &from, const string &to)
-// {
-//     vector<string> result;
-//     queue<Node> queue;
-//     vector<Node> node_history; // Behållning av alla noder som skapas
-
-//     Node begin{from, nullptr};
-//     queue.push(begin);
-//     node_history.push_back(begin); // Lägg till startnoden
-
-//     while (!queue.empty())
-//     {
-//         Node curr = queue.front();
-//         queue.pop();
-//         find_neighbors(dict, curr);
-
-//         for (const string &neighbor : curr.neighbors)
-//         {
-//             if (neighbor == to)
-//             {
-//                 // Vi har hittat målet, skapa resultatkedjan utan pekare
-//                 result.push_back(to);
-//                 Node temp = curr;
-//                 result.push_back(temp.word);
-
-//                 while (temp.word != from)
-//                 {
-//                     // Iterera bakåt genom att referera till node_history
-//                     temp = node_history[distance(node_history.begin(), find_if(node_history.begin(), node_history.end(), [&temp](Node const &n)
-//                                                                                { return n.word == temp.prev->word; }))];
-//                     result.push_back(temp.word);
-//                 }
-
-//                 reverse(result.begin(), result.end());
-//                 return result;
-//             }
-//             else
-//             {
-//                 dict.erase(neighbor);
-//                 Node next{neighbor, &node_history.back()}; // Skapa nästa nod utan dynamisk allokering
-//                 queue.push(next);
-//                 node_history.push_back(next); // Lägg till i historiklistan
-//             }
-//         }
-//     }
-
-//     reverse(result.begin(), result.end());
-//     return result;
-// }
-
+// BFS för att hitta den kortaste ordkedjan från 'from' till 'to'
 vector<string> find_shortest(Dictionary dict, const string &from, const string &to)
 {
-    vector<string> result;
-    queue<Node> queue;
+    queue<Node *> q;
+    q.push(new Node(from)); // Starta från 'from'
 
-    Node begin{from};
-    queue.push(begin);
-    while (!queue.empty())
+    // BFS-loop
+    while (!q.empty())
     {
-        Node curr = queue.front();
-        queue.pop();
-        find_neighbors(dict, curr);
-        for (string neighbor : curr.neighbors)
+        Node *curr = q.front();
+        q.pop();
+
+        // Hitta grannar till nuvarande ord
+        vector<string> neighbors;
+        find_neighbors(dict, *curr, neighbors);
+
+        for (const string &neighbor : neighbors)
         {
             if (neighbor == to)
             {
-                Node *curr_ptr = new Node(curr);
+                vector<string> result;
                 result.push_back(to);
-                result.push_back(curr_ptr->word);
-                while (curr_ptr->word != from)
+                Node *tmp = curr;
+
+                while (tmp != nullptr)
                 {
-                    result.push_back(curr_ptr->prev->word);
-                    curr_ptr = curr_ptr->prev;
+                    result.push_back(tmp->word);
+                    tmp = tmp->prev;
                 }
+
                 reverse(result.begin(), result.end());
                 return result;
             }
-            else
-            {
-                dict.erase(neighbor);
-                queue.push(Node{neighbor, new Node(curr)});
-            }
+            dict.erase(neighbor);             // Ta bort besökta ord
+            q.push(new Node(neighbor, curr)); // Lägg till i kö med länk till föregående nod
         }
     }
+    return {}; // Returnera tom vektor om ingen kedja hittades
+}
+
+// BFS för att hitta den längsta av alla kortaste ordkedjor som slutar med 'word'
+vector<string> find_longest(Dictionary dict, const string &word)
+{
+    queue<Node *> q;
+    q.push(new Node(word)); // Starta från 'word'
+
+    Node *last = nullptr;
+
+    // BFS-loop
+    while (!q.empty())
+    {
+        Node *curr = q.front();
+        q.pop();
+
+        // Hitta grannar till nuvarande ord
+        vector<string> neighbors;
+        find_neighbors(dict, *curr, neighbors);
+
+        for (const string &neighbor : neighbors)
+        {
+            dict.erase(neighbor); // Ta bort besökta ord
+            q.push(new Node(neighbor, curr));
+        }
+
+        last = curr; // Håll koll på den sista noden i BFS
+    }
+
+    vector<string> result;
+    while (last != nullptr)
+    {
+        result.push_back(last->word);
+        last = last->prev;
+    }
+
     reverse(result.begin(), result.end());
     return result;
 }
 
-/**
- * Hitta den längsta kortaste ordkedjan som slutar i 'word' i ordlistan 'dict'. Returvärdet är den
- * ordkedja som hittats. Det sista elementet ska vara 'word'.
- */
-vector<string> find_longest(Dictionary dict, const string &word)
-{
-    vector<string> result;
-    queue<Node> queue;
-
-    Node begin{word};
-    queue.push(begin);
-    while (true)
-    {
-        Node curr = queue.front();
-        queue.pop();
-        find_neighbors(dict, curr);
-        for (const string &neighbor : curr.neighbors)
-        {
-            dict.erase(neighbor);
-            queue.push(Node{neighbor, new Node(curr)});
-        }
-        if (queue.empty())
-        {
-            if (word != curr.word)
-            {
-                Node *curr_ptr = new Node(curr);
-                result.push_back(curr_ptr->word);
-                while (curr_ptr->prev->word != word)
-                {
-                    result.push_back(curr_ptr->prev->word);
-                    curr_ptr = curr_ptr->prev;
-                }
-            }
-            break;
-        }
-    }
-    result.push_back(word);
-    return result;
-}
-
-/**
- * Läs in ordlistan och returnera den som en vector av orden. Funktionen läser även bort raden med
- * #-tecknet så att resterande kod inte behöver hantera det.
- */
+// Läs in ordlistan från standard input
 Dictionary read_dictionary()
 {
     string line;
     vector<string> result;
-    while (std::getline(std::cin, line))
+
+    while (getline(cin, line))
     {
         if (line == "#")
-            break;
-
+            break; // Sluta läsa när vi når '#'
         result.push_back(line);
     }
 
     return Dictionary(result.begin(), result.end());
 }
 
-/**
- * Skriv ut en ordkedja på en rad.
- */
+// Skriv ut en ordkedja
 void print_chain(const vector<string> &chain)
 {
     if (chain.empty())
         return;
 
-    vector<string>::const_iterator i = chain.begin();
-    cout << *i;
-
-    for (++i; i != chain.end(); ++i)
-        cout << " -> " << *i;
-
+    cout << chain[0];
+    for (size_t i = 1; i < chain.size(); ++i)
+    {
+        cout << " -> " << chain[i];
+    }
     cout << endl;
 }
 
-/**
- * Skriv ut ": X ord" och sedan en ordkedja om det behövs. Om ordkedjan är tom, skriv "ingen lösning".
- */
+// Skriv ut svaret för en fråga
 void print_answer(const vector<string> &chain)
 {
     if (chain.empty())
@@ -235,17 +156,17 @@ void print_answer(const vector<string> &chain)
     }
 }
 
-/**
- * Läs in alla frågor. Anropar funktionerna "find_shortest" eller "find_longest" ovan när en fråga hittas.
- */
+// Läs in alla frågor och besvara dem
 void read_questions(const Dictionary &dict)
 {
     string line;
-    while (std::getline(std::cin, line))
+
+    while (getline(cin, line))
     {
         size_t space = line.find(' ');
         if (space != string::npos)
         {
+            // Fråga: hitta kortaste ordkedjan
             string first = line.substr(0, space);
             string second = line.substr(space + 1);
             vector<string> chain = find_shortest(dict, first, second);
@@ -255,6 +176,7 @@ void read_questions(const Dictionary &dict)
         }
         else
         {
+            // Fråga: hitta längsta av alla kortaste ordkedjor
             vector<string> chain = find_longest(dict, line);
 
             cout << line << ": ";
@@ -265,8 +187,8 @@ void read_questions(const Dictionary &dict)
 
 int main()
 {
-    Dictionary dict = read_dictionary();
-    read_questions(dict);
+    Dictionary dict = read_dictionary(); // Läs in ordlistan
+    read_questions(dict);                // Läs och besvara alla frågor
 
     return 0;
 }
